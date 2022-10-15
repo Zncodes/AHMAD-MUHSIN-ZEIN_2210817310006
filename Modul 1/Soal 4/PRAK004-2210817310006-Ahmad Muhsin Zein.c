ed9aa00f7d1639fd5786204072695b528adcda84
Zncodes <115259398+Zncodes@users.noreply.github.com>
2022-10-15 22:06:01 +0700

    Add files via upload

---
+++ Modul 1/Soal 4/PRAK004-2210817310006-Ahmad Muhsin Zein.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+   int baris;
+    for(baris = 0; baris < 33; baris++) {
+        printf("#");
+    }//looping #
+    printf("\n\n#\t\t\t        #\n");
+    printf("\n#       Ahmad Muhsin Zein       #\n");
+    printf("\n#         2210817310006         #\n");
+    printf("\n#\t\t\t        #\n\n");
+    for(baris = 0; baris < 33; baris++) {
+        printf("#");
+    }// looping #
+
+
+}
