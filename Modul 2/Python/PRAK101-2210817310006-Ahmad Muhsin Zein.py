d075f2464decdefd97eb44cf8823451fe107b769
Zncodes <115259398+Zncodes@users.noreply.github.com>
2022-10-15 18:51:25 +0700

    Delete Modul 2/Python directory

--- Modul 2/Python/PRAK101-2210817310006-Ahmad Muhsin Zein.py
+++
@@ -1,8 +0,0 @@
-x = 5 
-y = 7 
-z = 9
-j = x+y+z
-print("Variabel X bernilai ", x)
-print("Variabel Y bernilai ", y)
-print("Variabel Z bernilai ", z)
-print("Jumlah Variabel Tersebut Adalah ",j)
--- Modul 2/Python/PRAK106-2210817310006-Ahmad Muhsin Zein.py
+++
@@ -1,9 +0,0 @@
-a=4
-b=8
-c=3
-print("Variabel a bernilai ",a)
-print("Apakah a sama dengan b? Jawaban ",int(a==b))
-print("Variabel b bernilai ",b)
-print("Apakah b lebih besar dari c? Jawaban ",int(b>=c))
-print("Variabel c bernilai ",c)
-print("Apakah a tidak sama dengan c? Jawaban ",int(a!=c))
