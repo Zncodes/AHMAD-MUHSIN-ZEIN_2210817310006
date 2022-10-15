2937fc932088ed4a30b6e1b01641c945687c28f8
Zncodes <115259398+Zncodes@users.noreply.github.com>
2022-10-15 19:01:44 +0700

    Add files via upload

---
+++ Modul 2/Soal 6/PRAK106-2210817310006-Ahmad Muhsin Zein.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    a=4;
+    b=8;
+    c=3;
+    printf("Variabel a bernilai %d\n",a);
+    printf("Apakah a sama dengan b? ");
+    printf("Jawaban %d\n",a==b);
+    printf("Variabel b bernilai %d\n",b);
+    printf("Apakah b lebih besar dari c? ");
+    printf("Jawaban %d\n",b>=c);
+    printf("Variabel c bernilai %d\n",c);
+    printf("Apakah a tidak sama dengan c? ");
+    printf("Jawaban %d\n",a/=c);
+}
