13d77b08958067f39cd51e1a586e5829a65cd1c9
Zncodes <115259398+Zncodes@users.noreply.github.com>
2022-10-15 19:04:41 +0700

    Add files via upload

---
+++ Modul 2/Soal 8/PRAK108-2210817310006-Ahmad Muhsin Zein.py
@@ -0,0 +1,6 @@
+phi = 3.14
+kel = 5
+tempuh = 14
+jari2 = (tempuh/kel)*0.5/phi
+print("Diketahui :\nPak Dengklek mengelilingi taman = 5\nPutaran Jarak tempuh Pak Dengklek =",tempuh,"Kilometer")
+print("\nJawaban :\nJari-jari taman yang dikelilingi Pak Dengklek adalah",round(jari2,2),"Kilometer")
