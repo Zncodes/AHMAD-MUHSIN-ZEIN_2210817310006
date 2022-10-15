596132d0dd0013598b3ba93259bca79cc66ae83b
Zncodes <115259398+Zncodes@users.noreply.github.com>
2022-10-15 18:58:50 +0700

    Add files via upload

---
+++ Modul 2/Soal 4/PRAK104-2210817310006-Ahmad Muhsin Zein.py
@@ -0,0 +1,10 @@
+A =400000
+B =350000
+DA = 13
+DB = 21
+HA = A - DA/100 * A
+HB = B - DB/100 * B
+print("Harga Sepatu A adalah ",A)
+print("Harga Sepatu B adalah ",B)
+print("sepatu A mendapat diskon 13% sehingga harganya menjadi ",round(HA))
+print("sepatu B mendapat diskon 21% sehingga harganya menjadi ",round(HB))
