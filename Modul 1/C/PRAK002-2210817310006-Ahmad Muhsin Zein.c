87c841a9cf895fb69d2a9b571da89d0ee9a3cd81
Zncodes <115259398+Zncodes@users.noreply.github.com>
2022-10-15 16:17:13 +0700

    Add files via upload

---
+++ Modul 1/C/PRAK002-2210817310006-Ahmad Muhsin Zein.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Selamat Pagi, Ahmad Muhsin Zein\n");
+    printf("Selamat Siang, Ahmad Muhsin Zein\n");
+    printf("Selamat Malam, Ahmad Muhsin Zein\n");
+    return 0;
+
+}
