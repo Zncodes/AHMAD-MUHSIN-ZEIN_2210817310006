81aa4d55708b8ac0206f4936687eb2c7d8a58d14
Zncodes <115259398+Zncodes@users.noreply.github.com>
2022-10-15 18:59:59 +0700

    Add files via upload

---
+++ Modul 2/Soal 5/PRAK105-2210817310006-Ahmad Muhsin Zein.py
@@ -0,0 +1,10 @@
+a = 9
+b = 5
+x = 8
+y = 8
+j = (a%b)+(x%y)
+print("Variable a bernilai ",a)
+print("Variable b bernilai ",b)
+print("Variable x bernilai ",x)
+print("Variable y bernilai ",y)
+print("Total sisa bagi dari a bagi b dan x bagi y adalah ",j)
